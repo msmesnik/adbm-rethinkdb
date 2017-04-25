85149c2129d1d1095792f191ece1351b02d5bbf6
Michael Smesnik <ms@tailored-apps.com>
2017-04-25 16:14:28 +0200

    implement init

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-async function init ({ db, dbName, metadata, logger }) {
+async function init ({ db: r, dbName, metadata, logger }) {
+  const databases = await r.dbList().run()
 
+  if (!databases.includes(dbName)) {
+    logger.info('Creating database %s.', dbName)
+
+    await r.dbCreate(dbName).run()
+  } else {
+    logger.debug('Database %s already exists.', dbName)
+  }
+
+  const tables = await r.db(dbName).tableList().run()
+
+  if (!tables.includes(metadata)) {
+    logger.info('Creating metadata table %s.', metadata)
+
+    await r.db(dbName).tableCreate(metadata).run()
+  } else {
+    logger.debug('Metadata table %s already exists.', metadata)
+  }
 }
 
-async function getCompletedMigrationIds ({ db, metadata, logger }) {
+async function getCompletedMigrationIds ({ db: r, metadata, logger }) {
   const completed = []
 
   logger.debug('○ Found %s completed migrations in metadata table "%s".', completed.length, metadata)
@@ -10,10 +28,10 @@ async function getCompletedMigrationIds ({ db, metadata, logger }) {
   return completed.map(({ id }) => id)
 }
 
-async function registerMigration ({ id, db, metadata }) {
+async function registerMigration ({ id, db: r, metadata }) {
 }
 
-async function unregisterMigration ({ id, db, metadata }) {
+async function unregisterMigration ({ id, db: r, metadata }) {
 }
 
 module.exports = {
